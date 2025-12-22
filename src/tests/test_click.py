7ff94e0f84d97a18252f16d6b4cfab1f36627f9a
Derek Wan <d.wan@icloud.com>
2025-12-22 18:53:17 +0900

    2025-12-22 18:53:17 (Mon)  > DW-Mac  > derekwan

--- src/tests/test_click.py
+++ src/tests/test_click.py
@@ -2,16 +2,16 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from click import argument, command, echo, option
+from click import argument, command, echo
 from click.testing import CliRunner
 from hypothesis import given
 from typed_settings import Secret
 from utilities.hypothesis import paths, text_ascii
 from utilities.os import temp_environ
+from utilities.text import strip_and_dedent
 
 import restic.click
 import restic.repo
-from restic.click import Repo
 from restic.repo import SFTP, Backblaze
 
 if TYPE_CHECKING:
@@ -60,7 +60,16 @@ class TestRepo:
 
         result = CliRunner().invoke(cli, args=[backblaze.repository])
         assert result.exit_code == 2
-        assert result.stdout == f"repo = {backblaze}\n"
+        expected = strip_and_dedent(
+            f"""
+            Usage: cli [OPTIONS] REPO
+            Try 'cli --help' for help.
+
+            Error: Invalid value for 'REPO': For a Backblaze repository {backblaze.repository!r}, the environment varaibles 'BACKBLAZE_KEY_ID' and 'BACKBLAZE_APPLICATION_KEY' must be defined
+        """,
+            trailing=True,
+        )
+        assert result.stderr == expected
 
     @given(
         user=text_ascii(min_size=1),
